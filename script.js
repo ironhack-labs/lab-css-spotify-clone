bed74564847267b3285270303675aa10a56e7123
marcosAlvarezCalabria <marcosalvarezcalabria@gmail.com>
2023-12-05 16:47:14 +0000

    corregido

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const hamburguer=document.querySelector(".nav_toggle")
 const navMenu=document.querySelector(".nav-menu")
+const navMenuItem=document.querySelector(".nav-menu-item")
+const alert=document.querySelector("alert")
 
 hamburguer.addEventListener("click",()=>{
     
